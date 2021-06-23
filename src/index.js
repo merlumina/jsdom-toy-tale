4f27eb3c9c19fc9e06a735580371fc99ecd5ba83
Liz Wilson <lizriderwilson@gmail.com>
2021-06-23 13:25:30 -0400

    commit tree 3637456cd01b2be047844299bf4194850ff5a93d

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const toyCollection = document.getElementById('toy-collection');
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toys => resp.json())
 });
+
+function createToyInfoCard(toyArray) {
+  for (const toy of toyArray) {
+    const toyCard = document.createElement('div').setAttribute('class', 'card');
+    const cardName = document.createElement('h2');
+    const cardImg = document.createElement('img').setAttribute('class', 'toy-avatar');
+    const cardLikes = document.createElement('p');
+    const cardLikeBtn = document.createElement('button').setAttribute('class', 'like-btn');
+    const cardElements = [cardName, cardImg, cardLikes, cardLikeBtn];
+
+    cardName.innerText = toy.name;
+    cardImg.src = toy.image;
+    cardLikes.innerText = `${toy.likes} likes`;
+    cardLikeBtn.innerText = "Like <3";
+
+    for (const element of cardElements) {
+      toyCard.appendChild(element);
+    }
+
+    toyCollection.appendChild(toyCard);
+  }
+}
